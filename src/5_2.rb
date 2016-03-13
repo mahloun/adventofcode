ab8fb71a21ef872fcfd0a6913990cf5c8d6edf35
magneto <thms.no@gmail.com>
2016-03-13 22:15:46 +0100

    day5

---
+++ src/5_2.rb
@@ -0,0 +1,33 @@
+require_relative 'input'
+
+def has_at_least pattern, times
+  -> string { string.scan(pattern).length >= times }
+end
+
+def contains pattern
+  -> string { pattern =~ string }
+end
+
+def excludes pattern
+  -> string { not contains(pattern).call(string) }
+end
+
+#part1
+def has_three_vowels() has_at_least %r{[aeiou]}, 3 end
+def contains_twice_in_row() contains %r{(\w)\1} end
+def excludes_forbidden_strings() excludes %r{(?:ab|cd|pq|xy)} end
+
+#part2
+def contains_two_not_overlapping_pair() contains %r{(\w{2}).*\1} end
+def contains_bridge() contains %r{(\w).\1} end
+
+def is_nice? string
+  # has_three_vowels.call(string) \
+  #   && contains_twice_in_row.call(string) \
+  #   && excludes_forbidden_strings.call(string)
+  contains_bridge.call(string) && contains_two_not_overlapping_pair.call(string)
+end
+
+Input.open 5 do |input|
+  p input.select { |string| is_nice? string }.length
+end
--- src/input.rb
+++ src/input.rb
@@ -1,7 +1,7 @@
 module Input
   def self.open nb, &block
     File.open(File.join('input', "#{nb}_day"), 'r') do |file|
-      file.read.split("").tap(&block)
+      file.read.split("\n").tap(&block)
     end
   end
 end
